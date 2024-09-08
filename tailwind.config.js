d1a76c913fc13ccfeb8ec20e65c392cc942fdaee
jc_nana <joshuakt.mit@gmail.com>
2024-09-08 09:32:40 +0000

    added tailwindconfig

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.py"],
+  theme: {
+    container: {
+      screens: {}
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
